dd22204f6bb3cc368f11f7d66e990bafb96934b1
DESKTOP-FHKB46U\b056ff <kaka0817toika17@icloud.com>
2022-06-21 23:13:12 +0900

    それぞれの機能用のhtmlファイルを作成して、index.htmlから各ページに遷移できるようにしました。

--- ecoco/ecoco_app/views.py
+++ ecoco/ecoco_app/views.py
@@ -2,3 +2,18 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
   template_name = "index.html"
+
+class CalendarView(TemplateView):
+  template_name = "calendar.html"
+
+class SeparationView(TemplateView):
+  template_name = "separation.html"
+
+class SearchView(TemplateView):
+  template_name = "search.html"
+
+class PlaceView(TemplateView):
+  template_name = "place.html"
+
+class SettingView(TemplateView):
+  template_name = "setting.html"
