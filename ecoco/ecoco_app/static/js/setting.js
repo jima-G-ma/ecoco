3a583a780563cfbb1d9acad8f3ce3fa923f8f9be
DESKTOP-0J8OJIF\b047ff <b047ff@yamaguchi-u.ac.jp>
2022-06-25 00:09:34 +0900

    地域選択ができるようにしました

---
+++ ecoco/ecoco_app/static/js/setting.js
@@ -0,0 +1,14 @@
+function clickBtn(){
+
+	const area = document.form.area;
+
+	// 値(数値)を取得
+	const num = area.selectedIndex;
+	//const num = document.form1.color1.selectedIndex;
+
+	// 値(数値)から値(value値)を取得
+	const str = area.options[num].value;
+	//const str = document.form1.color1.options[num].value;
+
+	document.getElementById("selectedarea").textContent = str; 
+}
--- ecoco/ecoco_app/views.py
+++ ecoco/ecoco_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
   template_name = "index.html"
 
+
 class CalendarView(TemplateView):
   template_name = "calendar.html"
 
