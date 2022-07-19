bd6f1a20babffbba731d30dd5c443c12d4b71327
DESKTOP-FHKB46U\b056ff <kaka0817toika17@icloud.com>
2022-07-19 19:48:04 +0900

    設定ファイルをへんこうしました。

--- ecoco/ecoco/local_settings.py
+++ ecoco/ecoco/local_settings.py
@@ -1,1 +1,17 @@
 SECRET_KEY = 'hu&&7vdie(8#ds%zimz^-38c-gv295yec@vn#$waap58jy9brj' #追加
+
+import os
+from pathlib import Path
+
+#settings.pyからそのままコピー
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+#settings.pyからそのままコピー
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DEBUG = True #ローカルでDebugできるようになります
