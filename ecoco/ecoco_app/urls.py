10440430030a0b5d0de9a8d297ad97ccd01f3191
b047ff <b047ff@yamaguchi-u.ac.jp>
2023-11-24 14:57:36 +0900

    ログイン機能の追加

--- ecoco/ecoco_app/urls.py
+++ ecoco/ecoco_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . views import IndexView, CalendarView, SearchView, PlaceView,SettingView
+from . views import IndexView, CalendarView, SearchView, PlaceView, SettingView, MypageView
 from . views import bunbetsu
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('search/', SearchView.as_view()),
     path('place/', PlaceView.as_view()),
     path('setting/', SettingView.as_view()),
+    path('mypage/<int:pk>', MypageView.as_view(), name='mypage'),
 ]
